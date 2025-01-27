462573883e0efcfe2af5e0ab4b97163b97c237f1
Niraj Gupta <coder4117@gmail.com>
2025-01-27 20:19:28 +0530

    Nested Dynamic Routes

---
+++ nextjs-tutorial/src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+export default function Review() {
+  return (
+    <>
+      <h1>Review List</h1>
+      <h2>Review Product 1</h2>
+      <h2> Review Product 2</h2>
+      <h2>Review Product 3</h2>
+    </>
+  );
+}
