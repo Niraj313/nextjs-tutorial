ed7d72e9f2a9fe53ffface67960eda095d9395e3
Niraj Gupta <coder4117@gmail.com>
2025-01-28 20:16:33 +0530

    Not Found Page

--- nextjs-tutorial/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ nextjs-tutorial/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,11 @@
-export default async function ProductReview({ params,}: {
+import { notFound } from "next/navigation";
+
+export default async function ProductReview({ params, }: {
     params: Promise<{ productId: string, reviewId: string}>
 }) {
     const { productId, reviewId } = await params;
+    if (parseInt(reviewId) > 1000) {
+        notFound();
+    }
     return <h1>Review {reviewId} for Product {productId}</h1>
 }
