dc9876ab0e3c91c7caf3f36c89698f32e33d98e3
Niraj Gupta <coder4117@gmail.com>
2025-01-29 19:59:56 +0530

    Link Component

--- nextjs-tutorial/src/app/products/page.tsx
+++ nextjs-tutorial/src/app/products/page.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link"
 export default function Home() {
+    const productId = 100;
     return (
         <>
+            <Link href="/">home</Link>
             <h1>Product List</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
-
+            <Link href="/products/1">
+                <h2>Product 1</h2>
+            </Link>
+            <Link href="/products/2">
+                <h2>Product 2</h2>
+            </Link>
+            <Link href="/products/3">
+                <h2>Product 3</h2>
+            </Link>
+            <Link href={`/products/${productId}`}>
+                <h2>Product {productId}</h2>
+            </Link>
         </>
     )
 }
