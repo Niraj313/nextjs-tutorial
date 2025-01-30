70c8dd83c65c42afe1c8a1a760c383e092a12861
Niraj Gupta <coder4117@gmail.com>
2025-01-30 19:25:50 +0530

    Error Handling

--- nextjs-tutorial/src/app/products/[productId]/reviews/page.tsx
+++ nextjs-tutorial/src/app/products/[productId]/reviews/page.tsx
@@ -1,6 +1,13 @@
 /** @format */
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 
 export default function Review() {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   return (
     <>
       <h1>Review List</h1>
